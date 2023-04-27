430b7a3f2885ce4a5014638dd3676be600b3d2a3
SGabriel10 <sergio.gabriel.el10@gmail.com>
2023-04-27 04:30:27 -0400

    auth login
    remember me

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,6 +1,30 @@
 import React from 'react';
+import {useDispatch} from "react-redux";
+import {startLogin} from "../../actions/auth";
+import {useForm} from "../../hooks/useForm";
+import { useCookies } from 'react-cookie';
 
 const Login = () => {
+
+    const dispatch= useDispatch();
+    const [cookies, setCookie] = useCookies(['username','password']);
+    const [formLoginValues, handleInputChange ]= useForm( {
+        loginEmail: cookies.username,
+        loginPassword: cookies.password
+    });
+
+    const {loginEmail, loginPassword} = formLoginValues;
+
+    const handleLogin=(e)=>{
+        e.preventDefault();
+        dispatch(startLogin(loginEmail, loginPassword));
+    }
+
+    const handleRemerberMe=()=>{
+        setCookie('username', loginEmail, { path: '/' });
+        setCookie('password', loginPassword, { path: '/' });
+    }
+
     return (
         <div className="login-page">
             <div className="login-box">
@@ -14,7 +38,15 @@ const Login = () => {
 
                         <form action="../../index3.html" method="post">
                             <div className="input-group mb-3">
-                                <input type="email" className="form-control" placeholder="Email"/>
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    name="loginEmail"
+                                    value ={loginEmail}
+                                    onChange={handleInputChange}
+                                    placeholder="Email"
+
+                                />
                                     <div className="input-group-append">
                                         <div className="input-group-text">
                                             <span className="fas fa-envelope"></span>
@@ -22,7 +54,13 @@ const Login = () => {
                                     </div>
                             </div>
                             <div className="input-group mb-3">
-                                <input type="password" className="form-control" placeholder="Password"/>
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    name="loginPassword"
+                                    value ={loginPassword}
+                                    onChange={handleInputChange}
+                                    placeholder="Password"/>
                                     <div className="input-group-append">
                                         <div className="input-group-text">
                                             <span className="fas fa-lock"></span>
@@ -32,18 +70,21 @@ const Login = () => {
                             <div className="row">
                                 <div className="col-8">
                                     <div className="icheck-primary">
-                                        <input type="checkbox" id="remember"/>
+                                        <input onClick={handleRemerberMe} type="checkbox" id="remember"/>
                                             <label htmlFor="remember">
                                                 Remember Me
                                             </label>
                                     </div>
                                 </div>
 
-                                <div className="col-4">
-                                    <button type="submit" className="btn btn-primary btn-block">Iniciar Sesion</button>
-                                </div>
+
                             </div>
                         </form>
+
+                        <div className="social-auth-links text-center mt-2 mb-3">
+                                <button onClick={handleLogin} type="submit" className="btn btn-primary btn-block">Iniciar Sesion</button>
+                        </div>
+
 
                         <p className="mb-1">
                             <a href="forgot-password.html">I forgot my password</a>
